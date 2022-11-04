5053390876d6030acfd41764c39b4ae636c5fc8f
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-11-04 22:41:44 +0600

    toggle btn added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,13 +5,16 @@ import logo from '../utilities/images/logo.png'
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const toggleNav = () =>{
+        console.log('toggle clicked')
+    }
     return (
         <div className='header'>
             <div className="logo"> <Link to="/"><img src={logo} alt="Logo" /></Link></div>
 
             <Navbar></Navbar>
 
-            <div className="toggle-btn">
+            <div className="toggle-btn" onClick={toggleNav}>
                 <span className="bar"></span>
                 <span className="bar"></span>
                 <span className="bar"></span>
