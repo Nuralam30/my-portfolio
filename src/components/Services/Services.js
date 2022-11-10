aab8ee11483d41258f8ad8a65b874ae78f1bf438
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-11-10 17:13:58 +0600

    services page design

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import './Services.css';
 import Header from '../Header/Header';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { AllServices } from './allServices';
 
 const Services = () => {
+
+    console.log(AllServices)
+
     return (
         <div>
             <Header></Header>
-            <h2>This is services page</h2>
+            <div className="services">
+                <p className="service-text">Works as a Freelancer on Fiverr, Upwork, Linkedin & other marketplaces based on Bangladesh. Highly experienced in Frontend Website Design & Fixing any CSS & JS issues.</p>
+                <div className="service">
+                    {
+                        AllServices.map( ser =>
+                            <Card sx={{ maxWidth: 345 }} className='service-card'>
+                                <CardMedia
+                                    component="img"
+                                    alt="green iguana"
+                                    height="140"
+                                    image={ser.image}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                    {ser.heading}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                        )
+                    }
+                    
+                    
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/components/Services/allServices.js
@@ -0,0 +1,39 @@
+import Web from '../utilities/images/services/web.png';
+import Wordpress from '../utilities/images/services/wordpress.png';
+import BugFix from '../utilities/images/services/bugFix.png';
+import Graphic from '../utilities/images/services/graphic.png';
+import Ecommerce from '../utilities/images/services/ecommerce.png';
+import Settings from '../utilities/images/services/settings.png';
+
+export const AllServices = [
+    {
+        image : Web,
+        heading : 'Web Design',
+        description : ''
+    },
+    {
+        image : Wordpress,
+        heading : 'Wordpress Customization',
+        description : ''
+    },
+    {
+        image : BugFix,
+        heading : 'Bug Fixing',
+        description : ''
+    },
+    {
+        image : Graphic,
+        heading : 'Graphic Design',
+        description : ''
+    },
+    {
+        image : Ecommerce,
+        heading : 'Ecommerce Website Design',
+        description : ''
+    },
+    {
+        image : Settings,
+        heading : 'Settings & Secuirity',
+        description : ''
+    }
+]
