0d80673d98c45d2a49933148d6c523fa4cc75a05
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-11-12 19:18:15 +0600

    filter btns added

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,7 +6,17 @@ const Portfolio = () => {
     return (
         <div>
             <Header></Header>
-            <h2>This is portfolio page</h2>
+            <div className="portfolio">
+                <p className="my-text">Working as a Frontend developer with HTML5, CSS3, BOOTSTARP, JQUERY, REACT JS, ANGULAR JS, MATERIAL UI etc. Providing my freelance services on various market places. </p>
+
+                <ul className="filter-services">
+                    <li className='active'><div className='filter-animation'></div><button className='filter-btn'> ALL </button></li>
+                    <li><div className='filter-animation'></div><button className='filter-btn'> HTML </button></li>
+                    <li><div className='filter-animation'></div><button className='filter-btn'> WORDPRESS </button></li>
+                    <li><div className='filter-animation'></div><button className='filter-btn'> REACT JS </button></li>
+                    <li><div className='filter-animation'></div><button className='filter-btn'> FRONT-END </button></li>
+                </ul>
+            </div>
         </div>
     );
 };
