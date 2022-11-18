239358525e276134267875bc2998dfb43fa8e71c
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-11-18 22:59:44 +0600

    about page created

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import './About.css';
 import Header from '../Header/Header';
+import { styled } from '@mui/material/styles';
+import { Grid } from '@mui/material';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 
 const About = () => {
+
+
+    const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+        height: 10,
+        borderRadius: 5,
+        [`&.${linearProgressClasses.colorPrimary}`]: {
+            backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+        },
+        [`& .${linearProgressClasses.bar}`]: {
+            borderRadius: 5,
+            backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+        },
+    }));
+
+
     return (
-        <div>
+        <div className='about'>
             <Header></Header>
             <div className="intro">
                 <p className="intro-text">Hello, My name is Nura Alam and I am a junior Frontend Developer working on various frontend design for 3-4 years. I have already completed my frontend designing of various websites. I also provide my services on wordpress customizaton and bug fixing in (html,css and js)  </p>
+            </div>
+            <div className="skills-educations">
+                <Grid container spacing={2}>
+                    <Grid xs={12} md={6}>
+                        <div className="skills">
+                            <BorderLinearProgress variant="determinate" value={50} />
+                        </div>
+                    </Grid>
+                    <Grid xs={12} md={6}>
+                        <div className="educations">
+                            <h2>Hello</h2>
+                        </div>
+                    </Grid>
+                </Grid>
             </div>
         </div>
     );
---
+++ src/components/About/Qualifiatios.js
@@ -0,0 +1,27 @@
+
+export const Qualifications = [
+    {
+        id: 1,
+        degree: 'Web Design and Development',
+        year: '2020 | LEDP, LOT-01 | Batch-09 ',
+        description: 'I have completed a 200 hours course on Web Design and Development under LEDP, an ICT Division platform based on Bangladesh.'
+    },
+    {
+        id: 2,
+        degree: 'B. Sc.(Engineering) in ECE',
+        year: '2017-2020 | HSTU | Dinajpur',
+        description: 'I have completed my under graduate degree on Electronics and Communication Engineering from Hajee Mohammad Danesh Science and Technology University'
+    },
+    {
+        id: 3,
+        degree: 'The Complete React Redux Node Express MySQL Developer Course',
+        year: 'Aug 15, 2021 | Udemy',
+        description: 'It was my first learning on Udemy. I have completed this 8 hours course on MERN stack Development.'
+    },
+    {
+        id: 4,
+        degree: 'JavaScript, Bootstrap & PHP Certification for Beginners',
+        year: 'June 19, 2021 | Udemy',
+        description: 'This course was an beginner course on Fullstack Web Development. I have completed this 6 hours lengthy course on 2021.'
+    }
+]
