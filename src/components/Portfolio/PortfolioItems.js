c2336d84a5b2c918d304a549b37be46af0f1ddbc
Nuralam30 <nuralam.hstu.930@gmail.com>
2023-01-17 09:41:16 +0600

    new item added in protfolio

--- src/components/Portfolio/PortfolioItems.js
+++ src/components/Portfolio/PortfolioItems.js
@@ -4,6 +4,7 @@ import CSS2  from '../utilities/images/portfolio/css/css2.png';
 import Wordpress1  from '../utilities/images/portfolio/wordpress1.jpg';
 import html1 from '../utilities/images/portfolio/html/e-school.JPG';
 import html2 from '../utilities/images/portfolio/html/hot-gadgets.JPG';
+import figma1 from '../utilities/images/portfolio/figma/figma1.png';
 
 
 export const PortfolioItems = [
@@ -52,6 +53,14 @@ export const PortfolioItems = [
         name: 'Hot Gadgets',
         image: html2,
         link: 'https://nuralam30.github.io/hot-gadgets/',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus porro consequuntur sapiente dicta beatae, repudiandae natus vitae, iste quibusdam similique minima obcaecati aut voluptate illum ullam numquam vel tempore eos libero. Doloribus minima tenetur quia corporis assumenda, ea cum molestiae cupiditate ratione iusto tempore itaque ab voluptatibus minus expedita, hic nemo numquam, labore voluptate quam odit. Voluptates minus reiciendis quae ut, perferendis, praesentium laudantium a inventore saepe eius illo est, similique dolore ipsa dignissimos fugit ipsam. Laboriosam quo facere autem voluptatum quaerat voluptatem quas qui magni repudiandae? Dolorum doloremque ut beatae sequi eius, obcaecati eum deleniti eaque neque facere optio libero voluptates consectetur quaerat aliquam repellendus similique odio? Consequatur quos laudantium, error exercitationem totam nihil doloribus a laboriosam aspernatur maxime vero dicta necessitatibus nesciunt repudiandae fuga molestias praesentium. Accusantium tenetur impedit rem atque amet nemo minima aliquid quaerat placeat molestias reprehenderit, tempore laborum laboriosam et neque fuga assumenda ex error?'
+    },
+    {
+        id: 7,
+        category: 'figma',
+        name: 'Landing Page Design',
+        image: figma1,
+        link: 'https://www.figma.com/file/Yyw2aEvulIzoD82c2qk7ka/Untitled?node-id=0%3A1&t=dmeyBMDH2Bm1REhD-0',
         description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus porro consequuntur sapiente dicta beatae, repudiandae natus vitae, iste quibusdam similique minima obcaecati aut voluptate illum ullam numquam vel tempore eos libero. Doloribus minima tenetur quia corporis assumenda, ea cum molestiae cupiditate ratione iusto tempore itaque ab voluptatibus minus expedita, hic nemo numquam, labore voluptate quam odit. Voluptates minus reiciendis quae ut, perferendis, praesentium laudantium a inventore saepe eius illo est, similique dolore ipsa dignissimos fugit ipsam. Laboriosam quo facere autem voluptatum quaerat voluptatem quas qui magni repudiandae? Dolorum doloremque ut beatae sequi eius, obcaecati eum deleniti eaque neque facere optio libero voluptates consectetur quaerat aliquam repellendus similique odio? Consequatur quos laudantium, error exercitationem totam nihil doloribus a laboriosam aspernatur maxime vero dicta necessitatibus nesciunt repudiandae fuga molestias praesentium. Accusantium tenetur impedit rem atque amet nemo minima aliquid quaerat placeat molestias reprehenderit, tempore laborum laboriosam et neque fuga assumenda ex error?'
     },
 ]
